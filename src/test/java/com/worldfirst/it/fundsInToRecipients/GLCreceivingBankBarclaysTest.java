56f663bacde4ab7e097179e253e751be5b8dea0d
Ola Ajala <ola.ajala@worldfirst.com>
2018-06-05 16:39:44 +0100

    added GLCreceivingBankBarclaysTest test case

---
+++ src/test/java/com/worldfirst/it/fundsInToRecipients/GLCreceivingBankBarclaysTest.java
@@ -0,0 +1,66 @@
+package com.worldfirst.it.fundsInToRecipients;
+
+import static io.restassured.RestAssured.*;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import com.worldfirst.it.common.PayloadConverter;
+import com.worldfirst.it.common.RestUtilities;
+import com.worldfirst.it.constants.EndPoints;
+import com.worldfirst.it.constants.PathForTransfer;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class GLCreceivingBankBarclaysTest {
+	
+	private static Logger log = LogManager.getLogger(GLCreceivingBankBarclaysTest.class.getName());
+	RequestSpecification requestSpec;
+	ResponseSpecification responseSpec;
+	Response quoteResponse, bookResponse;
+	String quoteId;
+
+	@BeforeMethod
+	public void setUp() {
+		requestSpec = RestUtilities.postRequestSpecification();
+		responseSpec = RestUtilities.postResponseSpecification();
+	}
+	
+	@Test
+	public void quoteGLCReceivingBankBarclaysTest() {
+		log.info("***Quote Test***");
+		String requestBody = PayloadConverter.generatePayloadString("GLCClientSendsUSDBuyEUR.json");
+		quoteResponse = 
+		given()
+			.spec(RestUtilities.addPayloadToSpec(requestSpec, requestBody))
+		.when()
+			.post(EndPoints.QUOTE)
+		.then()
+			.spec(responseSpec)
+			.log().ifError()
+			.extract().response();	
+		String getQuoteresponse = RestUtilities.getQuoteResponse(quoteResponse);
+		JsonPath quoteJsonPath = RestUtilities.jsonParser(getQuoteresponse);
+		log.info(getQuoteresponse);
+		quoteId = quoteJsonPath.get("id");
+	}
+	
+	@Test (enabled = true, dependsOnMethods = {"quoteGLCReceivingBankBarclaysTest"})
+	public void bookGLCReceivingBankBarclaysTest() {
+		log.info("***Book Test***");
+		bookResponse = 
+		given()
+			.spec(RestUtilities.createPathParam(requestSpec, "quoteId", quoteId))
+			.basePath(PathForTransfer.TRANSFER)
+		.when()
+			.post(EndPoints.BOOK)
+		.then()
+			.spec(responseSpec)
+			.log().ifError()
+			.extract().response();
+		String getBookresponse = RestUtilities.getQuoteResponse(bookResponse);
+		log.info(getBookresponse);
+	}
+}
