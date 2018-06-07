77576c903122bc69e0b2b8968338f631349333fd
Ola Ajala <ola.ajala@worldfirst.com>
2018-06-07 09:24:19 +0100

    update credentials

--- src/main/java/com/worldfirst/it/constants/Auth.java
+++ src/main/java/com/worldfirst/it/constants/Auth.java
@@ -3,6 +3,6 @@ package com.worldfirst.it.constants;
 public class Auth {
 	
 	public static final String USER = "WFO";
-	public static final String PASSWORD = "ahx9jai1uafah2Mo";
+	public static final String PASSWORD = "ahx9jai1uafah2M";
 	
 }
